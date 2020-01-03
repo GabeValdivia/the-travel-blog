27245defeb7fa28156dd9692b66043f378f98ec2
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-02 20:36:09 -0800

    Add sample posts to WordPress backend. Write WP_Query loop to grab post titles

--- themes/thetravelblog/front-page.php
+++ themes/thetravelblog/front-page.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
     <h2 class="text-center">Latest Enteries</h2>
+    <section class="container content">
+        <div class="row">
+            <?php $args= array(
+                'posts_per_page' => 6
+            ); ?>
+
+            <?php $entries = new WP_Query($args); while($entries->have_posts() ): $entries->the_post(); ?>
+
+                <h1><?php the_title(); ?></h1>
+
+            <?php endwhile; wp_reset_postdata(); ?>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
