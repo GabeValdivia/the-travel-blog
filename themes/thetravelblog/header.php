2c6b0ecab97c0a7277ae334c74ecec99077555ca
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-03 23:10:58 -0800

    Add elseif condition to header page title for category. If on category page print that title

--- themes/thetravelblog/header.php
+++ themes/thetravelblog/header.php
@@ -48,6 +48,9 @@
                         <?php if(is_front_page()): ?>
                             <?php $description = get_bloginfo('description', 'display'); ?>
                             <h1 class="title"><span><?php echo $description; ?></span></h1>
+                        <?php elseif (is_category() ):  ?>
+                            <h1 class="title"><span><?php echo single_cat_title(); ?></span></h1>
+
                         <?php else: ?>
                             <h1 class="title"><span><?php echo the_title(); ?></span></h1>
                         <?php endif; ?>
