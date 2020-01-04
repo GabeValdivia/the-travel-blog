deb5c8a51b3ab59ae2b6ea6114146a493c5fb9ae
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-03 21:30:11 -0800

    Add Ninja Forms plugin. Add form to /contact/. Write if(is_page('contact')) conditional to set content width only on /contact/

--- themes/thetravelblog/page.php
+++ themes/thetravelblog/page.php
@@ -2,12 +2,24 @@
 
     <section class="container content">
         <div class="row">
+
+        <?php if(is_page('contact')): ?>
+            <div class="col-sm-8 col-sm-offset-2">
+                <?php
+                    while(have_posts()): the_post( );
+                    the_content();
+    
+                    endwhile;
+                ?>
+            </div>
+        <?php else: ?>
         <?php             
             while(have_posts()): the_post( );
                 the_content();
 
             endwhile;            
             ?>
+        <?php endif; ?>
         </div>
     </section>
 
